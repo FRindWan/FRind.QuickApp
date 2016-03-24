f5d8a673829f6baf902b4804ef54b40763f81f43
FRindWan <frindwan@sohu.com>
2016-03-24 18:37:30 +0800

    First Add

---
+++ QucikApp/Domain/Entites/IAggregateRoot.cs
@@ -0,0 +1,22 @@
+﻿/*-------------------------------------------------------------------------
+ * 作者：FRind
+ * 创建时间： 2016/3/24 星期四 18:35:13
+ * 版本号：v1.0
+ * 本类主要用途描述：
+ *  -------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QucikApp.Domain.Entites
+{
+    /// <summary>
+    /// <see cref="IAggregateRoot"/>
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+}
---
+++ QucikApp/Domain/Entites/IEntity.cs
@@ -0,0 +1,31 @@
+﻿/*-------------------------------------------------------------------------
+ * 作者：FRind
+ * 创建时间： 2016/3/24 星期四 18:32:32
+ * 版本号：v1.0
+ * 本类主要用途描述：
+ *  -------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QucikApp.Domain.Entites
+{
+    /// <summary>
+    /// <see cref="IEntity"/>
+    /// </summary>
+    public interface IEntity<TKey>
+    {
+        TKey ID { get; set; }
+    }
+
+    /// <summary>
+    /// 框架提供的默认Entity实体
+    /// </summary>
+    public interface IEntity : IEntity<Guid>
+    { 
+    
+    }
+}
